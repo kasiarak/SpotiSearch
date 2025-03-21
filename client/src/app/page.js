e40e0e2f6349391aa7a99355b7e3a671b1378d52
Katarzyna Rak <katarzynarak9@gmail.com>
2025-03-21 02:06:06 +0100

    Add top tracks, albums, and events info on homepage

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -6,6 +6,13 @@ export default function Home() {
     <div className={styles.page}>
       <h1>SpotiSearch</h1>
       <SearchBar/>
+      <div className={styles.infoContainer}>
+        <div className={styles.infoBox}>Check out the most popular songs from this artist and discover their biggest hits!</div>
+        <div className={styles.separator}></div>
+        <div className={styles.infoBox}>Explore the artist's discography and listen to their best albums.</div>
+        <div className={styles.separator}></div>
+        <div className={styles.infoBox}>Find upcoming concerts and events to see this artist live!</div>
+    </div>
     </div>
   );
 }
